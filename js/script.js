731ebddea8e1817c5ed5068a90ed3a2912e1531c
Beatriz Marconi <beatrizmarconi.dev@gmail.com>
2022-01-12 12:19:43 -0300

    Criando um Array de games

--- js/script.js
+++ js/script.js
@@ -1,3 +1,76 @@
 function cards(nome) {
   alert(`Vc clicou no Filme: ${nome}`);
 }
+
+let games = [
+  {
+    id:1,
+    nome:"Donkey Kong Country",
+    descrição:"jogo se passa em Donkey Kong Island onde é explorado seis regiões com ambientes diferentes,Donkey Kong e seu sobrinho Diddy Kong, que devem recuperar seu tesouro de bananas roubado por King K. Rool e os Kremlings.",
+    genero:"Plataforma",
+
+  },
+  {
+    id:2,
+    nome:"Crash Bandicoot",
+    descrição:"bandicoot geneticamente alterado Crash, luta para impedir o planos do cientista do mal Neo Córtex e seus lacaios",
+    genero:"Aventura,Plataforma",
+
+  },
+  {
+    id:3,
+    nome:"Apex Legends",
+    descrição:"os jogadores formam esquadrões de dois ou três jogadores e selecionam personagens predefinidos com habilidades distintas. O jogo possui dois modos. Em Battle Royale, até vinte esquadrões de três pessoas ou trinta duplas de duas pessoas pousam em uma ilha e procuram por armas e suprimentos para derrotar outros jogadores em combate. ",
+    genero:"Battle royale,Tiro em primeira pessoa",
+
+  },
+  {
+    id:4,
+    nome:"Fortnite",
+    descrição:"jogo free-to-play, onde até 100 jogadores lutam em espaços cada vez menores para serem a última pessoa ou time vencedor",
+    genero:"battle royale",
+
+  },
+  {
+    id:5,
+    nome:"Grand Theft Auto V",
+    descrição:"O jogo se passa no estado ficcional de San Andreas, com a história da campanha um jogador seguindo três criminosos e seus esforços para realizarem assaltos sob a pressão de uma agência governamental.",
+    genero:"Ação-aventura",
+
+  },
+  {
+    id:6,
+    nome:"Minecraft",
+    descrição:"os jogadores exploram um mundo aberto tridimensional intencionalmente em blocos, pixelizado e gerado proceduralmente, podendo descobrir e extrair matérias-primas, ferramentas artesanais, construir estruturas ou terraplenagens e, dependendo do modo de jogo, podem combater inimigos controlados por computador,",
+    genero:"Sandbox, sobrevivência",
+
+  },
+  {
+    id:7,
+    nome:"The Last of Us",
+    descrição:"os jogadores controlam Joel, um homem encarregado de escoltar uma adolescente chamada Ellie através de um Estados Unidos pós-apocalíptico.",
+    genero:"Ação-aventura,Sobrevivência",
+
+  },
+  {
+    id:8,
+    nome:"Pac-Man",
+    descrição:"o jogador era uma cabeça redonda com uma boca que se abre e fecha, posicionado em um labirinto simples repleto de pastilhas e 4 fantasmas que o perseguiam.",
+    genero:"Labirinto",
+
+  },
+  {
+    id:9,
+    nome:"Boogerman",
+    descrição:"um milionário excêntrico com hábitos higiênicos ruins. Suas armas como super-herói incluem flatulência, ranho e arrotos.",
+    genero:"Plataforma",
+
+  },
+  {
+    id:10,
+    nome:"Super Bomberman",
+    descrição:"O objetivo principal, é completar as fases depositando bombas em ordens e lugares estratégicos para destruir obstáculos e inimigos",
+    genero:"ação,",
+
+  }
+]
