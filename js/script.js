e6d9cb025ba09cf0aca42317022f23074bb2b2e8
Beatriz Marconi <beatrizmarconi.dev@gmail.com>
2022-01-18 17:11:18 -0300

    alterado cores e font

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
-function cards(nome) {
-  alert(`Vc clicou no Jogo: ${nome}`);
+function cards(descrição) {
+  alert(`${descrição}`);
 }
 
 let games = [
@@ -9,6 +9,7 @@ let games = [
     nome:"Donkey Kong Country",
     descrição:"jogo se passa em Donkey Kong Island onde é explorado seis regiões com ambientes diferentes,Donkey Kong e seu sobrinho Diddy Kong, que devem recuperar seu tesouro de bananas roubado por King K. Rool e os Kremlings.",
     genero:"Plataforma",
+    tipo: "retrô"
 
   },
   {
@@ -17,6 +18,7 @@ let games = [
     nome:"Crash Bandicoot",
     descrição:"bandicoot geneticamente alterado Crash, luta para impedir o planos do cientista do mal Neo Córtex e seus lacaios",
     genero:"Aventura,Plataforma",
+    tipo:"retrô"
 
   },
   {
@@ -25,6 +27,7 @@ let games = [
     nome:"Apex Legends",
     descrição:"os jogadores formam esquadrões de dois ou três jogadores e selecionam personagens predefinidos com habilidades distintas. O jogo possui dois modos. Em Battle Royale, até vinte esquadrões de três pessoas ou trinta duplas de duas pessoas pousam em uma ilha e procuram por armas e suprimentos para derrotar outros jogadores em combate. ",
     genero:"Battle royale,Tiro em primeira pessoa",
+    tipo:"Battle royale"
 
   },
   {
@@ -33,6 +36,7 @@ let games = [
     nome:"Fortnite",
     descrição:"jogo free-to-play, onde até 100 jogadores lutam em espaços cada vez menores para serem a última pessoa ou time vencedor",
     genero:"battle royale",
+    tipo:"Battle royale"
 
   },
   {
@@ -65,6 +69,7 @@ let games = [
     nome:"Pac-Man",
     descrição:"o jogador era uma cabeça redonda com uma boca que se abre e fecha, posicionado em um labirinto simples repleto de pastilhas e 4 fantasmas que o perseguiam.",
     genero:"Labirinto",
+    tipo:"retrô"
 
   },
   {
@@ -73,6 +78,7 @@ let games = [
     nome:"Boogerman",
     descrição:"um milionário excêntrico com hábitos higiênicos ruins. Suas armas como super-herói incluem flatulência, ranho e arrotos.",
     genero:"Plataforma",
+    tipo:"retrô"
 
   },
   {
@@ -81,27 +87,32 @@ let games = [
     nome:"Super Bomberman",
     descrição:"O objetivo principal, é completar as fases depositando bombas em ordens e lugares estratégicos para destruir obstáculos e inimigos",
     genero:"ação,",
+    tipo:"retrô"
 
   }
 ]
+let retros = games.filter(retro=>{
+  return retro.tipo == "retrô"
+});
 
 let userHtml= '';
 
-let boxDeCards = document.querySelector('#boxDeCards');
+let boxDeCardsretro = document.querySelector('#boxDeCardsretro');
 
-games.forEach(game =>{
+retros.forEach(gamer =>{
   userHtml+= `
-        <li>
-          <a onclick="cards('${game.nome}')" href="#">
+        <li class="a">
+          <a onclick="cards('${gamer.descrição}')" href="#">
             <div class="card aligment-column alignment-vert alignment position color-link">
-              <div class="alignment-vert grey-background alignment center position"><img src="${game.image}"></div>
-              <div class="paragraph center"><P> ${game.nome} </P></div>
+              <div class="alignment-vert grey-background alignment center position"><img src="${gamer.image}"></div>
+              <div class="paragraph center">
+                <P class= "teste"> ${gamer.nome} </P>
+                <P class= "teste2"> ${gamer.descrição} </P>
+              </div>
             </div>
           </a>
         </li>
   `;
   });
-
-boxDeCards.innerHTML = userHtml;
-  
+boxDeCardsretro.innerHTML = userHtml;
 
